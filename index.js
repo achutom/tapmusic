8b58270b38f6208fbfbae191925561ef1e9f2127
Asher Thomas <72754631+achutom@users.noreply.github.com>
2025-05-04 21:07:32 +0530

    Added keyboard shortcuts and some minor changes
    Added some keyboard shortcuts to tap the pads, now you could click the pads by just clicking the number keys on your keyboard.  Cleaned some unused code. I also added my Linkedln badge to webstite.

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
-window.addEventListener('load', () => {
-    const sounds = document.querySelectorAll(".sound");
-    const pads = document.querySelectorAll(".pads div");
-    const visual = document.querySelector(".visual");
-    const colors = [
-        "#60d394",
-        "#d36060",
-        "#c060d3",
-        "#6860d3",
-        "#90ec17",
-        "#e2d406"
-    ];
+let mediaRecorder;
+let audioChunks = [];
+const sounds = document.querySelectorAll(".sound");
+const pads = document.querySelectorAll(".pads div");
 
-    //ADD sound
-    pads.forEach((pad, index) =>{
-        pad.addEventListener('click', function(){
-           sounds[index].currentTime = 0;
-           sounds[index].play();
-
-           createBubbles(index);
-        });
+window.addEventListener("load", () => {
+  //ADD sound
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function () {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
     });
-
-    //function bubbles
-    const createBubbles = (index) => {
-        const bubble = document.createElement("div");
-        visual.appendChild(bubble);
-        bubble.style.backgroundColor = colors[index];
-        bubble.style.animation = "jump 1s ease";
-        bubble.addEventListener('animationend', function(){
-            visual.removeChild(this);
-        });
-    };
+  });
 });
 
+// Add keyboard shortcuts
+
+window.addEventListener("keydown", (event) => {
+  const key = event.key;
+
+  if (key >= 1 && key <= 6) {
+    const padIndex = parseInt(key) - 1;
+
+    if (pads[padIndex]) {
+      pads[padIndex].click();
+    }
+  }
+});
